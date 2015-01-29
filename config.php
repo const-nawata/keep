5168a1192e43d64c3d82f9a0f39c4ac924b6c9d9
Constantine <nawataster@gmail.com>
2015-01-29 15:37:40 +0200

    Config file commint

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Configuration file
+ * Created by Constantine Nawata (nawata@mail.ru) on 28-06-2010.
+ */
+
+define ( '_EX', 'Caught exception: ' );
+define( '_ASSORT_IMG_PATH', 'img/assortment/' );
+
+global $_SESSION;
+global $gl_Path; $gl_Path = './';
+global $gl_Xajax, $dbh, $log_obj;
+
+$host		= 'localhost';
+$db_name	= 'keep';
+$db_user	= 'root';
+$user_pass	= '043';//At work
+// $user_pass	= '0043';//At home
